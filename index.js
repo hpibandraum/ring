2b0900aeb66963c5d0c6135bc9e89509c5e9fc54
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-02-24 19:57:37 +0100

    added error message

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 function ringTheBell() {
-   var xhttp;
-   if (window.XMLHttpRequest) {
-     xhttp = new XMLHttpRequest();
-   } else {
-     xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-   }
+  var knopka = document.getElementById("knopka");
+  var xhttp;
+  if (window.XMLHttpRequest) {
+   xhttp = new XMLHttpRequest();
+  } else {
+   xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
    
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -13,14 +14,16 @@ function ringTheBell() {
       var secondsToWait = response.return_value;
       if (secondsToWait === parseInt(secondsToWait, 10)) {
         // check for integer from http://stackoverflow.com/a/14636652/1320237
-        var knopka = document.getElementById("knopka");
         knopka.innerHTML= "Ringing...";
         setTimeout(function() {
             knopka.innerHTML= "";
           }, secondsToWait * 1000);
-      }
+      } 
+    } else {
+      knopka.innerHTML= "Error - plug it in!";
     }
   };
+  knopka.innerHTML = "Sending...";
   xhttp.open("POST", "https://api.particle.io/v1/devices/25001c000247343138333038/bell?access_token=a2882f03a5972ba19227709f65c0f49d5091d9ba", true);
   xhttp.send();
 }
