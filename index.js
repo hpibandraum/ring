e2b3e6ac2da1bf4695ffcd5059dac9809f0d6238
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-04-12 09:59:13 +0200

    added error message

--- index.js
+++ index.js
@@ -9,21 +9,25 @@ function ringTheBell() {
   }
    
   xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
+    // xhttp.readyState
+    // http://www.w3schools.com/Ajax/ajax_xmlhttprequest_onreadystatechange.asp
+    if (xhttp.readyState == 4) {
       var response = JSON.parse(xhttp.responseText);
-      var secondsToWait = response.return_value;
-      if (secondsToWait === parseInt(secondsToWait, 10)) {
-        // check for integer from http://stackoverflow.com/a/14636652/1320237
-        knopka.innerHTML= "Ringing...";
-        setTimeout(function() {
-            knopka.innerHTML= "";
-          }, secondsToWait * 1000);
-      } 
-    } else {
-      knopka.innerHTML= "Error - plug it in!";
+      if (response.ok) {
+        var secondsToWait = response.return_value;
+        if (secondsToWait === parseInt(secondsToWait, 10)) {
+          // check for integer from http://stackoverflow.com/a/14636652/1320237
+          knopka.innerText= "Ringing...";
+          setTimeout(function() {
+              knopka.innerText= "Push to ring again.";
+            }, secondsToWait * 1000);
+        } 
+      } else {
+        knopka.innerText= "Error: " + response.error;
+      }
     }
-  };
-  knopka.innerHTML = "Sending...";
+  }
+  knopka.innerText = "Sending...";
   xhttp.open("POST", "https://api.particle.io/v1/devices/25001c000247343138333038/bell?access_token=a2882f03a5972ba19227709f65c0f49d5091d9ba", true);
   xhttp.send();
 }
